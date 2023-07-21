b05179e6a6771f0f2f6be830c4373f27f8e90d59
Sockenschuh <94380222+Sockenschuh@users.noreply.github.com>
2023-07-21 11:10:46 +0200

    all else

---
+++ createOverlay.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+# Define the dimensions
+width, height = 3000, 3000
+
+# Create a new image with white background
+img = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+imgin = Image.open('merged.png').convert('RGBA')
+pixels = img.load()
+pixelsin = imgin.load()
+for y in range(height):
+    for x in range(width):
+        if x%3 == 1 and y%3==1:
+            pixels[x, y] = pixelsin[x,y]
+
+img.save('overlay.png')
